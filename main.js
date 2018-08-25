4d6e9c296326ed78562d1fa08af11a2556924a81
[CYBOR] <muhametshin.1997@gmail.com>
2018-08-25 22:28:50 +0300

    Completelly implemented&prepared for publishing

--- main.js
+++ main.js
@@ -30,27 +30,32 @@ function loadComic(url) {
     req.open('GET', url);
     req.send();
     req.onloadend = () => {
-        if (!currentComic) {
-            currentComic = document.getElementById('currentComic');
-            currentComic.onclick = randomComic;
+        if (req.status === 200) {
+            if (!currentComic) {
+                currentComic = document.getElementById('currentComic');
+                currentComic.onclick = randomComic;
 
-            document.getElementById('prevComicButton').onclick = prevComic;
-            document.getElementById('nextComicButton').onclick = nextComic;
-            document.getElementById('randomComicButton').onclick = randomComic;
+                document.getElementById('prevComicButton').onclick = prevComic;
+                document.getElementById('nextComicButton').onclick = nextComic;
+                document.getElementById('randomComicButton').onclick = randomComic;
+            }
+
+            currentComic.classList.remove('loading');
+
+            let currentComicURL = new RegExp('https:\\/\\/imgs\\.xkcd\\.com\\/comics\\/\\w*\\.png', 'gm').exec(req.responseText)[0];
+            currentComicNumber = parseInt(new RegExp('https:\\/\\/xkcd\\.com\\/(\\d*)\\/').exec(req.responseText)[1]);
+
+            let comicNumberElement = document.getElementById('comicNumber');
+            comicNumberElement.innerText = `#${currentComicNumber}`;
+            comicNumberElement.setAttribute('href', `https://xkcd.com/${currentComicNumber}`);
+
+            let shareThroughVKElement = document.getElementById('shareThroughVK');
+            shareThroughVKElement.onclick = () =>
+                window.open(`http://vk.com/share.php?url=https://xkcd.com/${currentComicNumber}`,
+                    "Share via VK", "menubar=0,status=0,height=500,width=500,position=center");
+
+            currentComic.setAttribute('src', currentComicURL);
         }
-
-        currentComic.classList.remove('loading');
-
-        let currentComicURL = new RegExp('https:\\/\\/imgs\\.xkcd\\.com\\/comics\\/\\w*\\.png', 'gm').exec(req.responseText)[0];
-        currentComicNumber = parseInt(new RegExp('https:\\/\\/xkcd\\.com\\/(\\d*)\\/').exec(req.responseText)[1]);
-
-        let comicNumberElement = document.getElementById('comicNumber');
-        comicNumberElement.innerText = `#${currentComicNumber}`;
-        comicNumberElement.setAttribute('href', `https://xkcd.com/${currentComicNumber}`);
-
-        let shareThroughVKElement = document.getElementById('shareThroughVK');
-        shareThroughVKElement.setAttribute('href', `http://vk.com/share.php?url=https://xkcd.com/${currentComicNumber}`);
-
-        currentComic.setAttribute('src', currentComicURL);
+        else setTimeout(loadComic, 500, url);
     }
 }
